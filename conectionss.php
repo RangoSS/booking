234cc6cb7b5bfae2b7ca11bc53afd27f3c4ced00
RangoSS <tshidaisa@outlook.com>
2019-05-10 11:26:11 +0200

    my external connection becase of session

---
+++ conectionss.php
@@ -0,0 +1,12 @@
+
+<?php
+$db_host='localhost';
+$db_name='phashions';
+$username='root';
+$db_password='';
+
+
+$connectDan=mysqli_connect($db_host,$username,$db_password,$db_name);
+echo 'ypu are well connected';
+
+?>
